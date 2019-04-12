f8f4be11adbaeaea40a9c4f0be0d143a69ded616
Olivier <olivier@toussaint.fr>
2019-04-12 09:44:17 +0200

    mini refactor

---
+++ base.php
@@ -0,0 +1,3 @@
+<?php
+$base = new PDO('mysql:host=localhost;dbname=tp2', 'root', '');
+$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
--- user_add.php
+++ user_add.php
@@ -1,11 +1,9 @@
 <?php
 session_start();
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/base.php';
 
 use App\Model\User;
-
-$base = new PDO('mysql:host=localhost;dbname=tp2', 'root', '');
-$base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
 $loader = new \Twig\Loader\FilesystemLoader('templates');
 $twig = new \Twig\Environment($loader);
